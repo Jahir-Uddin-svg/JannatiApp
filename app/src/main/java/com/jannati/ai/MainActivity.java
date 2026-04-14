47d4a7c0734314d91053c9febffc37cbb32c4402
Jahir-Uddin-svg <jahir.uddin.biochem@gmail.com>
2026-04-14 16:54:06 +0600

    fix: remove missing icons and add activity

---
+++ app/src/main/java/com/jannati/ai/MainActivity.java
@@ -0,0 +1,10 @@
+package com.jannati.ai;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void Bundle(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
